e82c81383abfb6ca8f1ee370ae2d25373dcf0a5c
Katsiaryna_Kaloshych <koloshych@gmail.com>
2021-08-05 17:23:27 +0300

    first commit

---
+++ task1/src/main/java/com/epam/training/CommandLineRunnerBean.java
@@ -0,0 +1,14 @@
+package com.epam.training;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CommandLineRunnerBean implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Hello World");
+    }
+}
